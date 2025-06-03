f5833abc6b320f984b53ff9c61be195df37812f9
WeiberKev <158856366+WeiberKev@users.noreply.github.com>
2025-06-03 11:01:11 -0500

    actualizacio de datos

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,67 @@
+package org.example;
+
+import java.time.LocalDateTimete;
+
+public class Main {
+    public static void master(String[] args) {
+
+        System.out.println("Variables escapa del salon");
+
+        int edadPersona;
+        double precioProducto;
+        int cantidadHijosFamilia;
+        double salarioTrabajador;
+        double alturaPersonaMetros;
+        boolean interruptorEncendido;
+        char generoPersona; // Ejemplo: 'M', 'F', 'O'
+        String matriculaVehiculo; // Inicia con letra, mejor String
+        int productosDisponiblesAlmacen;
+        double distanciaKilometros;
+        double temperaturaAmbiente;
+        String numeroTelefono; // Mejor String para mantener formato y ceros iniciales
+        String fechaNacimientoPersona; // Usar clase LocalDate para fechas
+        int codigoPostal;
+        int idProducto;
+        int puntajeExamen;
+        double velocidadAuto;
+        boolean esMayorEdad;
+        boolean puertaAbierta;
+        long poblacionMundial;
+        char letraExamen;
+        String palabra;
+        int estrellasCalificacion;
+        boolean archivoActivo;
+        double precioBilleteAvion;
+        int puntuacionVideojuego;
+        boolean tieneLicenciaConduccion;
+        double calificacionPromedioCurso;
+        String nombreCompletoPersona;
+        int cantidadLibrosBiblioteca;
+        String codigoBarrasProducto;
+        int porcentajeBateriaDispositivo;
+        double tipoCambioDivisas;
+        String numeroMatriculaEstudiante;
+        double puntajePromedioDeportista;
+        char caracterEspecial; // Por ejemplo '@'
+        double numeroMuyPequeno; // Ejemplo constante de Planck
+        boolean sensorFuncionando;
+        String horaEvento;
+        boolean clienteRegistrado;
+        String idClienteUnico;
+        double volumenRecipiente;
+        String nombreUsuarioApp;
+        String codigoProductoNoNumerico;
+        int cantidadVisitantesSitioWeb;
+        boolean pagoConfirmado;
+        double pesoPersona;
+        double notaFinalCurso;
+        char teclaPulsada;
+        String nombreEmpresa;
+
+
+
+
+
+
+    }
+}
